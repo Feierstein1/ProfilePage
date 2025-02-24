9d501e8557996ef5bb66d597a338ccc42e23b8c8
Kenny <50640060+Feierstein1@users.noreply.github.com>
2025-02-24 17:45:28 -0500

    added translucent background and fixed repeat issue

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,9 @@ import Resume from '../../components/resume/Resume';
 
 const ResumePage = () => {
   return (
-    <Resume />
+    <div className="flex flex-col items-centerspace-y-6 inset-0 bg-city bg-repeat-x bg-bottom mix-blend-overlay">
+      <Resume />
+    </div>
   );
 };
 
